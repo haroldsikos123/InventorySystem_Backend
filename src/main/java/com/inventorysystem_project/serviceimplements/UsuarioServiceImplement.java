c472532901bc761675231014c85e7dcee46db984
haroldsikos123 <jhonharoldsikosastete@gmail.com>
2025-10-25 22:48:55 -0500

    Update UsuarioServiceImplement.java

--- src/main/java/com/inventorysystem_project/serviceimplements/UsuarioServiceImplement.java
+++ src/main/java/com/inventorysystem_project/serviceimplements/UsuarioServiceImplement.java
@@ -4,6 +4,7 @@ import com.inventorysystem_project.entities.Usuario;
 import com.inventorysystem_project.repositories.UsuarioRepository;
 import com.inventorysystem_project.serviceinterfaces.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +15,15 @@ public class UsuarioServiceImplement implements IUsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void insert(Usuario usuario) {
+        if (usuario.getPassword() != null && !usuario.getPassword().startsWith("$2a$")) {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        }
+
         usuarioRepository.save(usuario);
     }
 
